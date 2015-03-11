253cfc730e86598ab019084d78896932f9e52457
john.lin <john.lin@tpv-tech.com>
2015-03-11 22:31:28 +0800

    modified validate.js and css

--- form-validate.js
+++ form-validate.js
@@ -21,39 +21,56 @@
          return;
      }
 
-     var valid_char_patern = new RegExp("^[!@$%^&*()?/<>\\~`{}\[\]]",g);
-     if (valid_char_patern.match($('name').val())) {
+     var valid_char_patern = /[!#￥@$%&()?<>\"\'\/\\、。，“”:;,.`~\^\*||]+/;
+     var email_char_patern = /[!#￥$%&()?<>\"\'\/\\、。，“”:;,`~\^\*||]+/;
+     if (valid_char_patern.test($('#name').val())) {
          // $('#name').requestFocus();
          alert('User name contain unsupported char!');
          return;
      }
 
-     var email_patern = new RegExp('[0-1a-zA-Z]+[.]*[0-1a-zA-Z]+@[0-1a-zA-Z]+[.0-1a-zA-Z]+');
-     if (!email_patern.match($('#email').val())) {
+     var email_patern = new RegExp('([0-9a-zA-Z]+[.]*[0-9a-zA-Z]+)@([0-9a-zA-Z]+[.]+[0-9a-zA-Z]+)');
+     if (email_char_patern.test($('#email').val())) {
+         // $('#name').requestFocus();
+         alert('User email contain unsupported char!');
+         return;
+     }
+     if (!email_patern.test($('#email').val())) {
          // $('#email').requestFocus();
          alert('Email is not correct!');
          return;
      }
 
-     if (valid_char_patern.match($('pwd').val())) {
-         $('#pwd').requestFocus();
+     var pwd_char_patern=/[\\]+/;
+     if (pwd_char_patern.test($('#pwd').val())) {
+         // $('#pwd').requestFocus();
          alert('Password contain unsupported char!');
          return;
-     }
-
+     }     
+     
      var pwd_val = $('#pwd').val();
-     var confirm_pwd_val = $('confirm_pwd').val();
+     var confirm_pwd_val = $('#confirm_pwd').val();
+     var name_val = $('#name').val();
+     var email_val = $('#email').val();
 
-     if (pwd_val === confirm_pwd_val) {
+
+     if (pwd_val.trim() === confirm_pwd_val.trim()) {
+         email_val = email_val.trim();
+         name_val = name_val.trim();
+         pwd_val = pwd_val.trim();
          // location.href = 'register.php';
-         $.post('register.php', {name: $('name').val(),pwd:pwd_val,email:$('#email').val()}, function(data, textStatus, xhr) {
+         $.post('register.php', {
+             name: name_val,
+             pwd: pwd_val,
+             email: email_val
+         }, function(data, textStatus, xhr) {
              /*optional stuff to do after success */
-             console.log("data"+data);
-             console.log("textStatus"+textStatus);
-             console.log("\nxhr"+xhr);
+             console.log("data" + data);
+             console.log("textStatus" + textStatus);
+             console.log("\nxhr" + xhr);
          });
      } else {
-         $('confirm_pwd').requestFocus();
+         // $('confirm_pwd').requestFocus();
          alert('Confirm password is not the same !');
          return;
      }
